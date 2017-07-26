5f7d56320d73c5c99570785484076390ed961cd9
Roberto Antelo <roberto_antelo@hotmail.com>
2017-07-26 06:36:37 -0500

    Add GET request to gsheet using axios

--- myscript.js
+++ myscript.js
@@ -1,3 +1,14 @@
+var localDB = [];
+
 function ready() {
-  console.log("Im ready");
+  getCSV();
+}
+
+function getCSV() {
+  axios.get("https://docs.google.com/spreadsheets/d/1N9iSbV7yMQWR886z6qOK5g9lLyAUHXHmu_6MsZne_Bs/pub?gid=507572218&single=true&output=csv")
+    .then(function(response) {
+      localDB = response.data;
+      console.log(localDB);
+    })
+
 }
